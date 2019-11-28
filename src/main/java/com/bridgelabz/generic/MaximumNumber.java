593a76f9f160cf5c5a42a40c7c8cbc1fd28f2bf8
mahendrakundare <mr.kundare@gmail.com>
2019-11-28 17:14:26 +0530

    Refactor: all three methods converted into one generic method

--- src/main/java/com/bridgelabz/generic/MaximumNumber.java
+++ src/main/java/com/bridgelabz/generic/MaximumNumber.java
@@ -1,46 +1,13 @@
 package com.bridgelabz.generic;
 
 public class MaximumNumber {
-
-    public static Integer toFindMaxNumber() {
-        Integer num1 = 200;
-        Integer num2 = 50;
-        Integer num3 = 1000;
-        Integer max=num1;
-        if (num2.compareTo(max)>0)
-            max=num2;
-        if (num3.compareTo(max)>0)
-            max=num3;
-        else
-            max=num1;
+    //generic method
+    public static <E extends Comparable<E>> E toFindMax(E a, E b, E c) {
+        E max = a;
+        if (b.compareTo(max)>0)
+            max=b;
+        if (c.compareTo(max)>0)
+            max=c;
         return max;
-    }
-
-    public static Float toFindMaxFloatNumber() {
-        Float num1 = 10.6f;
-        Float num2 = 35.8f;
-        Float num3 = 5.5f;
-        Float max = num1;
-        if (num2.compareTo(max)>0)
-            max=num2;
-        else if (num3.compareTo(max)>0)
-            max=num3;
-        else
-            max=num1;
-        return max;
-    }
-
-    public static String toFindMaxString() {
-        String name1 = "ironman";
-        String name2 = "batman";
-        String name3 = "superman";
-        String maxString = name1;
-        if (name2.compareTo(maxString)>0)
-            maxString=name2;
-        else if (name3.compareTo(maxString)>0)
-            maxString=name3;
-        else
-            maxString=name1;
-        return maxString;
     }
 }
--- src/test/java/com/bridgelabz/generic/MaxNumberTest.java
+++ src/test/java/com/bridgelabz/generic/MaxNumberTest.java
@@ -7,22 +7,23 @@ public class MaxNumberTest {
 
     @Test
     public void toTestMaximumNumber() {
+
         MaximumNumber maximumNumber = new MaximumNumber();
-        Integer result = maximumNumber.toFindMaxNumber();
-        Assert.assertEquals((Integer) 1000, result);
+        Integer result = maximumNumber.toFindMax(1,2,3);
+        Assert.assertEquals((Integer) 3, result);
     }
 
     @Test
     public void toTestMaximumFloatNumber() {
         MaximumNumber maximumNumber = new MaximumNumber();
-        Float result = maximumNumber.toFindMaxFloatNumber();
+        Float result = maximumNumber.toFindMax(10.6f,35.8f,5.5f);
         Assert.assertEquals((Float)35.8f,result);
     }
 
     @Test
     public void toTestMaximumString(){
         MaximumNumber maximumNumber = new MaximumNumber();
-        String result = maximumNumber.toFindMaxString();
+        String result = maximumNumber.toFindMax("ironman","batman","superman");
         Assert.assertEquals("superman",result);
     }
 }
